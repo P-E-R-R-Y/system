c623f723d2ef63d68736306e51f17aeec022de8c
Chouteau Perry <perry.chouteau@epitech.eu>
2025-02-20 00:10:54 +0100

    fix(type): make it simple.

--- type.hpp
+++ type.hpp
@@ -1,16 +1,10 @@
 /**
- *
- * File: Type.hpp
- * Created Date: Fr Mar 2023
- * Project: PERRY
- * Author: Perry Chouteau
- *
- * Last Modified: Fri Mar 10 2023
- * Modified By: Perry Chouteau
- *
- * Copyright (c) 2023-2033 Perry Chouteau
- *
- **/
+ * @file type.hpp
+ * @author @Perry-Chouteau (perry.chouteau@outlook.com)
+ * @brief define types 
+ * @version 0.1
+ * @date 2025-02-19
+ */
 
 #ifndef TYPE_HPP_
     #define TYPE_HPP_
@@ -20,102 +14,70 @@
     #elif __APPLE__
         #define OS "macOS"
         #include <sys/types.h>
-        #include <string>
-        #include <iostream>
-        //use __int32_t
-        //use float
     #else //if __linux__
         #define OS "Linux"
-        #include <sys/types.h>
-        #include <string>
-        #include <iostream>
     #endif
 
+    #include <string>
+    #include <iostream>
     #include <vector>
 
 namespace type {
 
-    #define __v1i_t __int32_t
-    #define __v1f_t float
+    //#define __uint8_t unsigned char
+    //#define __int8_t char
+    //#define __uint32_t unsigned int
+    //#define __int32_t int
 
-    //int32
-    typedef struct __v2u_s {
-        __uint32_t x;
-        __uint32_t y;
-    } __v2u_t;
+    template <typename T>
+    struct __v2_t {
+        T x;
+        T y;
+    };
 
-    typedef struct __v3u_s {
-        __uint32_t x;
-        __uint32_t y;
-        __uint32_t z;
-    } __v3u_t;
+    template <typename T>
+    struct __v3_t {
+        T x;
+        T y;
+        T z;
+    };
 
-    typedef struct __v4u_s {
-        __uint32_t x;
-        __uint32_t y;
-        __uint32_t w;
-        __uint32_t h;
-    } __v4u_t;
+    template <typename T>
+    struct __v4_t {
+        T x;
+        T y;
+        T w;
+        T h;
+    };
 
-    typedef struct __v2i_s {
-        __int32_t x;
-        __int32_t y;
-    } __v2i_t;
+    #define __v2u_t __v2_t<__uint32_t>
+    #define __v2i_t __v2_t<__int32_t>
+    #define __v2f_t __v2_t<float>
+    #define __v2d_t __v2_t<double>
 
-    typedef struct __v3i_s {
-        __int32_t x;
-        __int32_t y;
-        __int32_t z;
-    } __v3i_t;
+    #define __v3u_t __v3_t<__uint32_t>
+    #define __v3i_t __v3_t<__int32_t>
+    #define __v3f_t __v3_t<float>
+    #define __v3d_t __v3_t<double>
 
-    typedef struct __v4i_s {
-        __int32_t x;
-        __int32_t y;
-        __int32_t w;
-        __int32_t h;
-    } __v4i_t;
-
-    //double
-    typedef struct __v2f_s {
-        double x;
-        double y;
-    } __v2f_t;
-
-    typedef struct __v3f_s {
-        double x;
-        double y;
-        double z;
-    } __v3f_t;
-
-    typedef struct __v4f_s {
-        double x;
-        double y;
-        double w;
-        double h;
-    } __v4f_t;
+    #define __v4u_t __v4_t<__uint32_t>
+    #define __v4i_t __v4_t<__int32_t>
+    #define __v4f_t __v4_t<float>
+    #define __v4d_t __v4_t<double>
 
     typedef struct __color_s {
-        double r;
-        double g;
-        double b;
-        double a;
+        __uint8_t r;
+        __uint8_t g;
+        __uint8_t b;
+        __uint8_t a;
     } __color_t;
 
-    typedef struct __zone_s {
-        double top;
-        double left;
-        double front;
-        double bottom;
-        double right;
-        double back;
-    } __zone_t;
-
     typedef struct _quaternion_s {
+        double w;
         double x;
         double y;
         double z;
-        double w;
-    } _quaternion_t;
+    } __quaternion_t;
 
 } // namespace type
 
